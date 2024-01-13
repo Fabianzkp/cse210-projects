a410ba3472e58488b0056b9ed46725d05f3bad72
fabianzkp <fabianzkp@gmail.com>
2024-01-13 19:57:41 +0100

    recent prep 3 finished

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+
+        Console.Write("Please input a number");
+        int number = int.Parse(Console.ReadLine());
+        
+        while (number !=0)
+        {
+            Console.Write("Input another number");
+            
+        }
     }
 }
