1987a09910d71c0d8228c9ac10558f27b6c0fbe9
fabianzkp <fabianzkp@gmail.com>
2024-01-13 21:48:15 +0100

    finished prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
@@ -6,13 +8,66 @@ class Program
     {
         Console.WriteLine("Hello Prep4 World!");
 
-        Console.Write("Please input a number");
-        int number = int.Parse(Console.ReadLine());
-        
-        while (number !=0)
+        List<int> numbers = new List<int>();
+
+        while (true)
         {
-            Console.Write("Input another number");
-            
+            Console.Write("Please input a number (enter 0 to stop): ");
+            if (int.TryParse(Console.ReadLine(), out int num))
+            {
+                if (num == 0)
+                {
+                    break;
+                }
+                numbers.Add(num);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+        }
+
+        // Find the smallest positive number
+        int smallestPositive = numbers.Where(n => n > 0).DefaultIfEmpty(0).Min();
+        Console.WriteLine($"Smallest positive number: {smallestPositive}");
+
+        // Sort the list
+        numbers.Sort();
+
+        // Display the sorted list
+        Console.WriteLine("Sorted list:");
+        foreach (int number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+
+        // Calculating and displaying the sum
+        int sum = numbers.Sum();
+        Console.WriteLine($"\nSum = {sum}");
+
+        // Calculating and displaying the average
+        int count = numbers.Count;
+        if (count > 0)
+        {
+            float average = (float)sum / count;
+            Console.WriteLine($"Average = {average:F1}");
+        }
+        else
+        {
+            Console.WriteLine("Please input a number to calculate average.");
+        }
+
+        // Finding and displaying the maximum
+        if (count > 0)
+        {
+            int max = numbers.Max();
+            Console.WriteLine($"Maximum number = {max}");
+        }
+        else
+        {
+            Console.WriteLine("No numbers entered to find the maximum.");
         }
     }
 }
+
+
